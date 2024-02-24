2a6e55a20eda659f82b7c0179c5c10fb47b7ae4b
Yaroslav <yaroslav.p@genie-solution.com>
2024-02-25 01:10:21 +0200

    add min and ma[ filtering for price

--- Helpers/FilteringAndPagination/ColumnFilter.cs
+++ Helpers/FilteringAndPagination/ColumnFilter.cs
@@ -4,5 +4,7 @@
     {
         public string id { get; set; }
         public string value { get; set; }
+        public string lowerValue { get; set; }
+        public string upperValue { get; set; }
     }
 }
--- Helpers/FilteringAndPagination/CustomExpressionFilter.cs
+++ Helpers/FilteringAndPagination/CustomExpressionFilter.cs
@@ -4,55 +4,80 @@ namespace Shop.Helpers.FilteringAndPagination
 {
     public static class CustomExpressionFilter<T> where T : class
     {
-        public class ExpressionFilter
-        {
-            public string ColumnName { get; set; }
-            public string Value { get; set; }
-        }
         public static Expression<Func<T, bool>> CustomFilter(List<ColumnFilter> columnFilters, string className)
         {
             Expression<Func<T, bool>> filters = null;
             try
             {
-                var expressionFilters = new List<ExpressionFilter>();
-                foreach (var item in columnFilters)
-                {
-                    expressionFilters.Add(new ExpressionFilter() { ColumnName = item.id, Value = item.value });
-                }
                 // Create the parameter expression for the input data
                 var parameter = Expression.Parameter(typeof(T), className);
 
                 // Build the filter expression dynamically
                 Expression filterExpression = null;
-                foreach (var filter in expressionFilters)
+                foreach (var filter in columnFilters)
                 {
-                    var property = Expression.Property(parameter, filter.ColumnName);
+                    var property = Expression.Property(parameter, filter.id);
 
                     Expression comparison;
 
                     if (property.Type == typeof(string))
                     {
-                        var constant = Expression.Constant(filter.Value);
+                        var constant = Expression.Constant(filter.value);
                         comparison = Expression.Call(property, "Contains", Type.EmptyTypes, constant);
                     }
                     else if (property.Type == typeof(double))
                     {
-                        var constant = Expression.Constant(Convert.ToDouble(filter.Value));
-                        comparison = Expression.Equal(property, constant);
+                        if (!string.IsNullOrEmpty(filter.lowerValue) && !string.IsNullOrEmpty(filter.upperValue))
+                        {
+                            var lowerBound = Expression.Constant(Convert.ToDouble(filter.lowerValue));
+                            var upperBound = Expression.Constant(Convert.ToDouble(filter.upperValue));
+
+                            var lowerConstant = Expression.Constant(lowerBound);
+                            var upperConstant = Expression.Constant(upperBound);
+
+                            var greaterThanOrEqual = Expression.GreaterThanOrEqual(property, lowerConstant);
+                            var lessThanOrEqual = Expression.LessThanOrEqual(property, upperConstant);
+
+                            //// Combine two conditions using AndAlso
+                            comparison = Expression.AndAlso(greaterThanOrEqual, lessThanOrEqual);
+                        }
+                        else
+                        {
+                            var constant = Expression.Constant(Convert.ToDouble(filter.value));
+                            comparison = Expression.Equal(property, constant);
+                        }
                     }
                     else if (property.Type == typeof(decimal))
                     {
-                        var constant = Expression.Constant(Convert.ToDecimal(filter.Value));
-                        comparison = Expression.Equal(property, constant);
+                        if (!string.IsNullOrEmpty(filter.lowerValue) && !string.IsNullOrEmpty(filter.upperValue))
+                        {
+                            var lowerBound = Expression.Constant(Convert.ToDecimal(filter.lowerValue));
+                            var upperBound = Expression.Constant(Convert.ToDecimal(filter.upperValue));
+
+                            var lowerConstant = Expression.Constant(lowerBound);
+                            var upperConstant = Expression.Constant(upperBound);
+
+                            var greaterThanOrEqual = Expression.GreaterThanOrEqual(property, lowerConstant);
+                            var lessThanOrEqual = Expression.LessThanOrEqual(property, upperConstant);
+
+                            //// Combine two conditions using AndAlso
+                            comparison = Expression.AndAlso(greaterThanOrEqual, lessThanOrEqual);
+                        }
+                        else
+                        {
+                            var constant = Expression.Constant(Convert.ToDecimal(filter.value));
+                            comparison = Expression.Equal(property, constant);
+                        }
+
                     }
                     else if (property.Type == typeof(Guid))
                     {
-                        var constant = Expression.Constant(Guid.Parse(filter.Value));
+                        var constant = Expression.Constant(Guid.Parse(filter.value));
                         comparison = Expression.Equal(property, constant);
                     }
                     else
                     {
-                        var constant = Expression.Constant(Convert.ToInt32(filter.Value));
+                        var constant = Expression.Constant(Convert.ToInt32(filter.value));
                         comparison = Expression.Equal(property, constant);
                     }
 
