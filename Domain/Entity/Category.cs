d38a7c075d6c44951ef9a05619d3af34eda50a97
Yaroslav <yaroslav.p@genie-solution.com>
2024-02-25 10:11:00 +0200

    add categoris model

---
+++ Domain/Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿using Shop.Abstractions;
+
+namespace Shop.Domain.Entity
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+        public Guid? CategoryId { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
--- Domain/Entity/Product.cs
+++ Domain/Entity/Product.cs
@@ -7,6 +7,7 @@ namespace Shop.Domain.Entity
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-
+        public Guid CategoryId { get; set; }
+        public Guid UserId { get; set; }
     }
 }
