c47f5ebaacd83f601cee62097917c7e0e34087ed
Yaroslav <yaroslav.p@genie-solution.com>
2024-04-18 12:22:21 +0300

    add refresh token

--- BackApp/Controllers/AuthController.cs
+++ BackApp/Controllers/AuthController.cs
@@ -21,7 +21,7 @@ public class AuthController
     public async Task<BaseResponseModel<TokenModel>> Login(LoginRequestModel loginModel)
     {
 
-        var token = await _authService.Login(loginModel);
+        var token = await _authService.LoginAsync(loginModel);
 
         var response = new BaseResponseModel<TokenModel>();
         response.SetSuccessResponse(token);
@@ -29,4 +29,15 @@ public class AuthController
         return response;
     }
 
+    [HttpPost]
+    public async Task<BaseResponseModel<TokenModel>> RestoreAccessToken(RestoreAccessTokenRequestModel model)
+    {
+
+        var token = await _authService.RestoreAccessTokenAsync(model);
+
+        var response = new BaseResponseModel<TokenModel>();
+        response.SetSuccessResponse(token);
+
+        return response;
+    }
 }
